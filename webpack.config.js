05c93af7047f6c66cf0eabce33b8ebf2a705958d
twomeetings1 <hhxu@thoughtworks.com>
2020-09-21 16:47:01 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports={
-  entry:'./entry.js',
+  entry:'./src/index.js',
   output: {
-    path: path.resolve(__dirname,'./dist'),
-    filename:'my-bundle.js'
+    path: path.resolve(__dirname,'./lib'),
+    filename:'index.js',
+    libraryTarget:'commonjs2'
   },
   module:{
     rules:[
-      {test:/\.jsx?$/, use:'babel-loader'}
+      {test:/\.jsx?$/, use:'babel-loader'},
+      {test:/\.css$/, use:['style-loader', 'css-loader']}
     ]
   },
   plugins:[
