466f92184f7bf4f22d73e4dfbda3c7873f508bad
twomeetings1 <hhxu@thoughtworks.com>
2020-09-21 11:22:11 +0800

    add webpack and react

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Container = ()=><div>React Application</div>
+
+ReactDOM.render(<Container></Container>, document.getElementById('root'))
