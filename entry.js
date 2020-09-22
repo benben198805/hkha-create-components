af42a92e9ad356c50fcac627302299983fb72285
twomeetings1 <hhxu@thoughtworks.com>
2020-09-22 11:16:12 +0800

    update title

--- entry.js
+++ entry.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Container = () => <div>React Application</div>
+const Container = () => <div><h3>React Application</h3></div>
 
 ReactDOM.render(<Container />, document.getElementById('root'))
